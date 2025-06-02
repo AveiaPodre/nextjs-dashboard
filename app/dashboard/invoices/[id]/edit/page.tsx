12ef668bd9df44348d0b554eedf2cd11080fce3b
Averinaldo Júnior <av_jr@outlook.com.br>
2025-06-02 11:53:38 -0300

    error handling and acessibility

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,18 +1,29 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+import { validate as validateUUID } from 'uuid';
 
 export default async function Page(props: {
     params: Promise<{
         id: string
     }>
 }) {
-    const params = await props.params
-    const id = params.id
+    const params = await props.params;
+    const id = params.id;
+
+    if (!validateUUID(id)) {
+        notFound();
+    }
+
     const [invoice, customers] = await Promise.all([
         fetchInvoiceById(id),
         fetchCustomers(),
     ])
+
+    if(!invoice) {
+        notFound()
+    }
 
     return (
         <main>
